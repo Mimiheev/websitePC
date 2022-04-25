7f3e425996a702c53a4c47f29ed04d215f5f4761
Mimiheev <94638935+Mimiheev@users.noreply.github.com>
2022-04-25 18:41:38 +0300

    development of code for sending and collecting data from the form

--- JS/modal.js
+++ JS/modal.js
@@ -1,39 +1,43 @@
-const modalBtn = document.querySelector('.modal__button')
-const modal = document.querySelector('.modal')
-const modalInner = modal.querySelector('.modal__inner')
+const modal = () => {
+    const modalBtn = document.querySelector('.modal__button')
+    const modal = document.querySelector('.modal')
+    const modalInner = modal.querySelector('.modal__inner')
 
-modalInner.style.position = 'relative'
+    modalInner.style.position = 'relative'
 
-const addCloseBtn = () => {
-    const closeBtn = document.createElement('div')
+    const addCloseBtn = () => {
+        const closeBtn = document.createElement('div')
 
-    closeBtn.classList.add('close-button')
-    closeBtn.innerHTML = '&times'
-    closeBtn.style.position = 'absolute'
-    closeBtn.style.right = '10px'
-    closeBtn.style.top = '10px'
-    closeBtn.style.padding = '3px'
-    closeBtn.style.height = '20px'
-    closeBtn.style.width = '20px'
-    closeBtn.style.fontSize = '20px'
-    closeBtn.style.cursor = 'pointer'
+        closeBtn.classList.add('close-button')
+        closeBtn.innerHTML = '&times'
+        closeBtn.style.position = 'absolute'
+        closeBtn.style.right = '10px'
+        closeBtn.style.top = '10px'
+        closeBtn.style.padding = '3px'
+        closeBtn.style.height = '20px'
+        closeBtn.style.width = '20px'
+        closeBtn.style.fontSize = '20px'
+        closeBtn.style.cursor = 'pointer'
 
-    modalInner.append(closeBtn)
+        modalInner.append(closeBtn)
 
-    closeBtn.addEventListener('click', () => {
-        modal.style.display = ''
+        closeBtn.addEventListener('click', () => {
+            modal.style.display = ''
+        })
+    }
+
+    modalBtn.addEventListener('click', () => {
+        modal.style.display = 'flex'
     })
+
+    modal.addEventListener('click', (event) => {
+        const modalContent = event.target.closest('.modal__inner')
+        if (!modalContent) {
+            modal.style.display = ''
+        }
+    })
+
+    addCloseBtn()
 }
 
-modalBtn.addEventListener('click', () => {
-    modal.style.display = 'flex'
-})
-
-modal.addEventListener('click', (event) => {
-    const modalContent = event.target.closest('.modal__inner')
-    if (!modalContent) {
-        modal.style.display = ''
-    }
-})
-
-addCloseBtn()
+modal()
